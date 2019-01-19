a85c371591325308cf8dab4a765dd561dcddde74
ebi5714 <ebihara5714@gmail.com>
2019-01-19 23:21:47 +0900

    active storage実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,56 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update]
+
+  # GET /comments
+  def index
+    @comments = Comment.all
+  end
+
+  # GET /comments/1
+  def show
+  end
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.fetch(:comment, {}).permit(:content, :image)
+    end
+end
